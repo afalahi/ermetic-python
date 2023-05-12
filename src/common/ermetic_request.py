34e2ddfa97fab26c86a7d87d7f3a7ef57512df1d
Ali Falahi <ali@afalahi.com>
2023-05-12 08:08:00 -0600

    implemented error handling

--- src/common/ermetic_request.py
+++ src/common/ermetic_request.py
@@ -15,26 +15,50 @@ def ermetic_request(query: Callable):
         'Authorization': f'Bearer {config["ERMETIC_TOKEN"]}',
         'Content-Type': 'application/json'
     }
-    res = requests.post(url=URL, headers=HEADERS, json={
-        'query': query(CURRENT_CURSOR)})
-    data: dict = res.json()
-    res.close()
-
-    # Extract the resource name, whatever we're querying
-    resource = ''
-    for key in data['data']:
-        resource = key
-    # prepare pagination variables
-    results: List[Dict] = data['data'][resource]['nodes']
-    has_next_page: bool = data['data'][resource]['pageInfo']['hasNextPage']
-    CURRENT_CURSOR = f"\"{data['data'][resource]['pageInfo']['endCursor']}\""
-    # loop if we have more than one page
-    while (has_next_page):
+    try:
         res = requests.post(url=URL, headers=HEADERS, json={
             'query': query(CURRENT_CURSOR)})
-        data = res.json()
-        results += data['data'][resource]['nodes']
-        has_next_page = data['data'][resource]['pageInfo']['hasNextPage']
+        res.raise_for_status()
+        data: dict = res.json()
+        res.close()
+        resource = ''
+        for key in data['data']:
+            resource = key
+        # prepare pagination variables
+        results: List[Dict] = data['data'][resource]['nodes']
+        has_next_page: bool = data['data'][resource]['pageInfo']['hasNextPage']
         CURRENT_CURSOR = f"\"{data['data'][resource]['pageInfo']['endCursor']}\""
-    res.close()
-    return results
+        # loop if we have more than one page
+        while (has_next_page):
+            res = requests.post(url=URL, headers=HEADERS, json={
+                'query': query(CURRENT_CURSOR)})
+            res.raise_for_status()
+            data = res.json()
+            results += data['data'][resource]['nodes']
+            has_next_page = data['data'][resource]['pageInfo']['hasNextPage']
+            CURRENT_CURSOR = f"\"{data['data'][resource]['pageInfo']['endCursor']}\""
+        res.close()
+        return results
+    except requests.exceptions.HTTPError as error:
+        raise SystemExit(error)
+    except requests.exceptions.RequestException as error:
+        SystemExit(error)
+
+    # Extract the resource name, whatever we're querying
+    # resource = ''
+    # for key in data['data']:
+    #     resource = key
+    # # prepare pagination variables
+    # results: List[Dict] = data['data'][resource]['nodes']
+    # has_next_page: bool = data['data'][resource]['pageInfo']['hasNextPage']
+    # CURRENT_CURSOR = f"\"{data['data'][resource]['pageInfo']['endCursor']}\""
+    # # loop if we have more than one page
+    # while (has_next_page):
+    #     res = requests.post(url=URL, headers=HEADERS, json={
+    #         'query': query(CURRENT_CURSOR)})
+    #     data = res.json()
+    #     results += data['data'][resource]['nodes']
+    #     has_next_page = data['data'][resource]['pageInfo']['hasNextPage']
+    #     CURRENT_CURSOR = f"\"{data['data'][resource]['pageInfo']['endCursor']}\""
+    # res.close()
+    # return results
