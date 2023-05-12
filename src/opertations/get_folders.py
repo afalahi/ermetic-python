ae00e8da62d9c559c405e213a46d7401b81e508f
Ali Falahi <ali@afalahi.com>
2023-05-12 08:11:55 -0600

    reorg

--- src/opertations/get_folders.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from common.ermetic_request import ermetic_request
-from queries.get_folders_query import get_folders_query
-
-
-def get_folders():
-    folders = ermetic_request(query=get_folders_query)
-    return folders
