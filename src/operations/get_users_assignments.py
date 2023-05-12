6590ff40fcd9122b6d52230f13e6a35aa4590283
Ali Falahi <ali@afalahi.com>
2023-05-12 08:03:52 -0600

    CSV/JSON output | cleaned logic

--- src/operations/get_users_assignments.py
+++ src/operations/get_users_assignments.py
@@ -13,15 +13,16 @@
 # limitations under the License.
 
 from typing import List, Dict
-from pandas import DataFrame
 import json
+import csv
 from copy import deepcopy
+
 from operations.get_aws_accounts import get_aws_accounts
 from operations.get_folders import get_folders
 from operations.get_users import get_users
 
 
-def get_users_assignments():
+def get_users_assignments(csv_file=False, json_file=False):
     users = get_users()
     aws_accounts = get_aws_accounts(False)
     folders = get_folders()
@@ -30,51 +31,68 @@ def get_users_assignments():
     # root_ous = [r for r in folders if r['Id']
     #             == 'awsRoot' or r['Id'] == 'azureRoot' or r['Id'] == 'gcpRoot']
     # folder_hierarchy: List
-    access_report: List[Dict] = []
     # for idx, folder in enumerate(folders):
     #     if folder['Id'] == 'awsRoot' or 'azureRoot':
     #         pass
     #     for i, f in enumerate(folders, start=1):
     #         if folder['Id'] == f['ParentScopeId']:
     #             object = {"folderName": folder['Name']}
-    # for user in users:
-    #     if user['ScopeId'] is None:
-    #         user["ScopeId"] = 'Organization'
-    #     for folder in folders:
-    #         if folder['Id'] == user['ScopeId']:
-    #             user['ScopeName'] = folder['Name']
-    #             user['AccessType'] = 'Folder'
-    #     for account in aws_accounts:
-    #         if account['Id'] == user['ScopeId']:
-    #             user['ScopeName'] = account['Name']
-    #             user['AccessType'] = 'Direct'
-    #         else:
-    #             user['AccessType'] = 'inherited'
-    obj = {}
-    userRole = []
+    access_report: List[Dict] = []
+
     for account in aws_accounts:
+        obj = {}
+        userRole = []
+        userName = []
+        access_type = []
         obj["account"] = account['Name']
         obj["accountId"] = account['Id']
         for user in users:
             if account['Id'] == user['ScopeId']:
-                userRole.append(
-                    f'[{user["UserId"]}, {user["Role"]}, Direct]')
+                userName.append(user["UserId"])
+                userRole.append(user['Role'])
+                access_type.append('Direct')
+                # userRole.append(
+                #     f'[{user["UserId"]}, {user["Role"]}, Direct]')
+                # userRole.append(
+                #     {"userId": user['UserId'], "Role": user['Role'], "access": "Direct"})
                 obj['Users'] = userRole
             if user['ScopeId'] is None:
-                userRole.append(
-                    f'[{user["UserId"]}, {user["Role"]}, Inherited: Organization]')
+                userName.append(user["UserId"])
+                userRole.append(user['Role'])
+                access_type.append('Organization')
+                # userRole.append(
+                #     f'[{user["UserId"]}, {user["Role"]}, Organization]')
+                # userRole.append(
+                #     {"userId": user['UserId'], "Role": user['Role'], "access": "Organization"})
                 obj['Users'] = userRole
             for folder in folders:
-                if folder['Id'] == user['ScopeId']:
-                    userRole.append(
-                        f'[{user["UserId"]}, {user["Role"]}, Inherited: {folder["Name"]}]')
-                    obj['Users'] = userRole
+                if folder['Id'] == user['ScopeId'] and folder['Id'] == account['ParentScopeId']:
+                    userName.append(user["UserId"])
+                    userRole.append(user['Role'])
+                    access_type.append(folder["Name"])
+                    # userRole.append(
+                    #     f'[{user["UserId"]}, {user["Role"]}, {folder["Name"]}]')
+                    # userRole.append(
+                    #     {"userId": user['UserId'], "Role": user['Role'], "access": folder["Name"]})
+        obj['Users'] = userName
+        obj['Role'] = userRole
+        obj['AccessType'] = access_type
         access_report.append(deepcopy(obj))
-    # user_assignments: List[Dict] = []
+    if csv_file:
+        with open('users.csv', 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(access_report[0].keys())
+            for entry in access_report:
+                account = entry['account']
+                accountId = entry['accountId']
+                users = entry['Users']
+                role = entry['Role']
+                accessType = entry['AccessType']
+                for i in range(len(users)):
+                    writer.writerow(
+                        [account, accountId, users[i], role[i], accessType[i]])
+    if json_file:
+        with open(file='users.json', mode='w', newline='') as jsonFile:
+            jsonFile.write(json.dumps(users))
+            jsonFile.close()
     return access_report
-
-
-users = get_users_assignments()
-with open(file='users.json', mode='w', newline='') as jsonFile:
-    jsonFile.write(json.dumps(users))
-    jsonFile.close()
