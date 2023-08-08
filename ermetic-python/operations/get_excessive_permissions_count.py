d95bf5a6088986b5ee44b98626ee8e94077c3e4a
Ali Falahi <ali@afalahi.com>
2023-08-08 15:40:28 -0600

    installed pandas and xlsx | using pandas

--- ermetic-python/operations/get_excessive_permissions_count.py
+++ ermetic-python/operations/get_excessive_permissions_count.py
@@ -11,14 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import xlsxwriter
+
+import pandas as pd
 from datetime import datetime, timedelta
 from operations.get_aws_accounts import get_aws_accounts
 from operations.get_folders import get_folders
-from common import ermetic_request, save_to_csv, get_ermetic_folder_path
-from queries import aws_excessive_permissions
+from common.ermetic_request import ermetic_request
+from common.save_to_disk import save_to_csv
+from common.get_ermetic_folder_path import get_ermetic_folder_path
+from queries.findings_query import findings_query
 
-def get_aws_excessive_permissions_count(save_csv:bool=False, save_excel:bool=False, split_charts:bool=False, days:int=30):
+def get_aws_excessive_permissions_count(save_csv:bool=False, save_excel:bool=False, split_charts:bool=False, days:int=30, add_charts:bool=False, chart_trend:bool=False, compare:bool=False):
     if not save_excel and not save_csv:
         print('Cannot save report, you must provide one of these arguments: save_csv=True or save_excel=True')
         return
@@ -28,7 +31,9 @@ def get_aws_excessive_permissions_count(save_csv:bool=False, save_excel:bool=Fal
     print("getting permissions count")
     now = datetime.now()
     month_ago = now - timedelta(days=days)
-    filters = f'''Types:[
+    month_ago.strftime('%Y-%m-%dT%H:%M:%S')
+    def filters(): 
+        return f'''Types:[
         AwsInactiveUserFinding, 
         AwsInactiveRoleFinding,
         AwsExcessivePermissionGroupFinding, 
@@ -39,7 +44,7 @@ def get_aws_excessive_permissions_count(save_csv:bool=False, save_excel:bool=Fal
         CreationTimeStart:"{month_ago.strftime('%Y-%m-%dT%H:%M:%S')}"
         CreationTimeEnd:"{now.strftime('%Y-%m-%dT%H:%M:%S')}"
         '''
-    excessive_permissions = ermetic_request(aws_excessive_permissions,filters=filters)
+    excessive_permissions = ermetic_request(findings_query,filters=filters())
     permission_report = []
     count = 0
     for account in aws_accounts:
@@ -52,6 +57,7 @@ def get_aws_excessive_permissions_count(save_csv:bool=False, save_excel:bool=Fal
             "OverPrivilegedRoles":0,
             "OverPrivilegedUsers":0,
             "OverPrivilegedPermissionSet":0,
+            "Date": datetime.now().strftime('%Y-%m-%d')
         }
         for permission in excessive_permissions:
             if account['Id'] == permission['AccountId']:
@@ -76,79 +82,64 @@ def get_aws_excessive_permissions_count(save_csv:bool=False, save_excel:bool=Fal
     if save_csv:
         save_to_csv(data=permission_report, file_name="excessive_permissions")
     if save_excel:
-        # Create the workbook and sheet
+        # Create the workbook and sheet using Pandas DF
+        df = pd.DataFrame(permission_report)
         workbook_name = f"excessive_permissions_{now.strftime('%Y-%m-%d')}.xlsx"
         sheet_name = 'excessive_permissions_overview'
-        workbook = xlsxwriter.Workbook(workbook_name)
-        worksheet =workbook.add_worksheet(name=sheet_name)
-        #Get the headers
-        headers = permission_report[0].keys()
-        # get number of rows from the permission report list. This ensures the sheet is dynamic
-        num_rows = len(permission_report)
-
-        #Write sheet headers 
-        for col_num, header in enumerate(headers):
-            worksheet.write(0, col_num, header)
-
+        writer = pd.ExcelWriter(workbook_name, engine='xlsxwriter')
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+        workbook = writer.book
+        worksheet = writer.sheets[sheet_name]
         #Freeze the headers
         worksheet.freeze_panes(1, 0)
-
-        #Write sheet values
-        for row_num, row_data in enumerate(permission_report, start=1):
-            for col_num, (key, value) in enumerate(row_data.items()):
-                worksheet.write(row_num, col_num, value)
         # Set Column width to max with of header
-        for col_num, header in enumerate(headers, start=1):
-            max_length = len(str(header))  # Start with the length of the header
-            for row_data in permission_report:
-                cell_content = row_data[header]
-                max_length = max(max_length, len(str(cell_content)))  # Compare with the content of each row
-            worksheet.set_column(col_num - 1, col_num - 1, max_length + 1)  # Set the column width
-        # add the chart
-        chart = workbook.add_chart({'type': 'line'})
-        chart.set_size({'width': 800, 'height': 350}) 
-        chart_position = chr(65 + len(permission_report[0])) + '2'
-        chart_width = 13
-        charts_per_row = 2
-        last_data_column = len(headers) 
-        for col_num in range(1, len(headers)):
-            # Create a new chart object for each series
-            if split_charts:
-                chart = workbook.add_chart({'type': 'line'})
-                chart.set_size({'width': 800, 'height': 350}) 
-                # Add a series to the chart
-                chart.add_series({
-                    'values': f'={sheet_name}!${chr(65+col_num)}$2:${chr(65+col_num)}${num_rows+1}',
-                    'trendline':{
-                        'type': 'polynomial',
-                        'order': 3,
-                    },
-                    'categories': f'={sheet_name}!$A$2:$A${num_rows+1}',
-                    'name': f'{sheet_name}!${chr(65+col_num)}$1',
-                })
-                # Calculate the chart row and column position
-                chart_row = 2
-                chart_col = last_data_column + (col_num - 1) % charts_per_row * chart_width
-                chart_col_letter = chr(65 + chart_col)
-                chart_position = f'{chart_col_letter}{chart_row + (col_num - 1) // charts_per_row * 20}'
+        for col_num, value in enumerate(df.columns):
+            max_length = max(df[value].astype(str).apply(len).max(), len(str(value)))
+            worksheet.set_column(col_num, col_num, max_length + 1)
+        if add_charts:
+            # add the chart
+            chart = workbook.add_chart({'type': 'line'})
+            chart.set_size({'width': 800, 'height': 350}) 
+            chart_position = chr(65 + len(df.columns)) + '2'
+            chart_width = 13
+            charts_per_row = 2
+            last_data_column = len(df.columns)
+            for col_num in range(1, len(df.columns) - 1):
+                # Create a new chart object for each series
+                if split_charts:
+                    chart = workbook.add_chart({'type': 'line'})
+                    chart.set_size({'width': 800, 'height': 350}) 
+                    # Add a series to the chart
+                    chart.add_series({
+                        'values': f'={sheet_name}!${chr(65 + col_num)}$2:${chr(65 + col_num)}${len(df) + 1}',
+                        'categories': f'={sheet_name}!$A$2:$A${len(df) + 1}',
+                        'name': f'{sheet_name}!${chr(65 + col_num)}$1',
+                    })
+                    if chart_trend:
+                        chart.add_series({
+                            'trendline': {'type': 'polynomial', 'order': 3},
+                        })
+                    # Calculate the chart row and column position
+                    chart_row = 2
+                    chart_col = last_data_column + (col_num - 1) % charts_per_row * chart_width
+                    chart_col_letter = chr(65 + chart_col)
+                    chart_position = f'{chart_col_letter}{chart_row + (col_num - 1) // charts_per_row * 20}'
+                    worksheet.insert_chart(chart_position, chart)
+                else:
+                    chart.add_series({
+                        'values': f'={sheet_name}!${chr(65 + col_num)}$2:${chr(65 + col_num)}${len(df) + 1}',
+                        'categories': f'={sheet_name}!$A$2:$A${len(df) + 1}',
+                        'name': f'{sheet_name}!${chr(65 + col_num)}$1',
+                    })
+                    if chart_trend:
+                        chart.add_series({
+                            'trendline': {'type': 'polynomial', 'order': 3},
+                        })
+            if not split_charts:
+                # Configure the chart title and x and y axes
+                chart.set_title({'name': 'Excessive Permissions'})
+                chart.set_x_axis({'name': 'Accounts'})
+                chart.set_y_axis({'name': 'Categories'})
+                # Insert single sheet
                 worksheet.insert_chart(chart_position, chart)
-            else:
-                chart.add_series({
-                    'values': f'={sheet_name}!${chr(65+col_num)}$2:${chr(65+col_num)}${num_rows+1}',
-                    'trendline':{
-                        'type': 'polynomial',
-                        'order': 3,
-                    },
-                    'categories': f'={sheet_name}!$A$2:$A${num_rows+1}',
-                    'name': f'{sheet_name}!${chr(65+col_num)}$1',
-                })
-        # worksheet.insert_chart('' + str(15 * (col_num-1) + 2), chart)
-        # chart_position = chr(65 + len(permission_report[0])) + '2'
-        if not split_charts:
-            # Configure the chart title and x and y axes
-            chart.set_title({'name': 'Excessive Permissions'})
-            chart.set_x_axis({'name': 'Accounts'})
-            chart.set_y_axis({'name': 'Categories'})
-            # Insert single sheet
-            worksheet.insert_chart(chart_position, chart)
-        workbook.close()
+        writer.close()
