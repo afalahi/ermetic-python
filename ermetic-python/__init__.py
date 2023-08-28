706dfb48120c6341f859ced235ff44b095b0f4b2
Ali Falahi <ali@afalahi.com>
2023-08-28 10:39:13 -0600

    updated imports

--- ermetic-python/__init__.py
+++ ermetic-python/__init__.py
@@ -1,11 +1,11 @@
 # Copyright 2023 ali.falahi@ermetic.com
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,9 +18,6 @@ from operations.get_aws_accounts import get_aws_accounts
 from operations.get_okta_users import get_okta_users
 from operations.get_aws_billable_resources import get_aws_billable_resources
 from operations.get_excessive_permissions_count import get_aws_excessive_permissions_count
+from operations.Folders import Folders
+from common.ReportHandler import ReportHandler
 from Filters.FindingFilter import FindingFilter
-
-# get_aws_excessive_permissions_count(save_excel=True,days=90, add_charts=True, split_charts=True)
-finding_filter = FindingFilter(types="AwsCloudTrailTrailBucketEventDisabledFinding")
-finding_filter.types = ['AwsEc2SecurityGroupUnusedFinding']
-print(finding_filter.to_filter_string())
